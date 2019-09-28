ec6fc246a215650dc426fecdf1be34b0f897d4f4
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-09-28 17:52:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,10 @@
 def find_element_index(array, value_to_find)
   array.index (2)
+  if array.index (2) == 0 
+    do array.index (400)
  
 end
-def find_element_index(array, value_to_find)
-  array.index (400)
- 
-end
+
 
 
 def find_max_value(array)
