b869d290f85995c135b47a2a78fa1e981b506824
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-09-28 17:52:54 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,17 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.index (2)
+  if array.index (2) == 0 
+    do array.index (400)
+ 
 end
 
+
+
 def find_max_value(array)
-  # Add your solution here
+  array.max
+  
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min
 end
